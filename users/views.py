788d8f8ba5600744ee599e728705c2ea80a8b1d9
Lucas <123456789@qq.com>
2020-12-07 12:17:51 +0800

    注册案例:请求数据 2. 请求方式的获取

--- users/views.py
+++ users/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def register(request):
 
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print('username: %s password: %s' % (username, password))
+        return HttpResponse('进行注册业务处理')
+
 
